4b32f567d5ea9b7534965264ee73bc34fe6030d7
The Coding Canal <thecodingcanal@gmail.com>
2022-07-18 05:34:02 -0500

    Added blocks

--- kryptonitemod/src/main/java/com/codingcanal/kryptonitemod/KryptoniteMod.java
+++ kryptonitemod/src/main/java/com/codingcanal/kryptonitemod/KryptoniteMod.java
@@ -1,5 +1,6 @@
 package com.codingcanal.kryptonitemod;
 
+import com.codingcanal.kryptonitemod.block.ModBlocks;
 import com.codingcanal.kryptonitemod.item.ModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -28,6 +29,7 @@ public class KryptoniteMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
---
+++ kryptonitemod/src/main/java/com/codingcanal/kryptonitemod/block/ModBlocks.java
@@ -0,0 +1,50 @@
+package com.codingcanal.kryptonitemod.block;
+
+import com.codingcanal.kryptonitemod.KryptoniteMod;
+import com.codingcanal.kryptonitemod.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, KryptoniteMod.MODID);
+
+    public static final RegistryObject<Block> KRYPTONITE_BLOCK = registerBlock("kryptonite_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.CACTUS)
+                    .strength(3f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MISC);
+
+    public static final RegistryObject<Block> KRYPTONITE_ORE = registerBlock("kryptonite_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(5f).requiresCorrectToolForDrops(),
+                    UniformInt.of(3, 6)), CreativeModeTab.TAB_MISC);
+    public static final RegistryObject<Block> DEEPSLATE_KRYPTONITE_ORE = registerBlock("deepslate_kryptonite_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(5f).requiresCorrectToolForDrops(),
+                    UniformInt.of(4, 8)), CreativeModeTab.TAB_MISC);
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
